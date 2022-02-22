ff4af61ae31f9624fa859e9752c175d14004479b
Anita Chan <achan@C02FKAZ6MD6M.local>
2022-02-22 09:42:14 +0000

    Completed exercise 1-4, wip 5&6

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,41 +1,43 @@
 // useState: tic tac toe
 // http://localhost:3000/isolated/exercise/04.js
 
-import * as React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
-
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
-
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [winner, setWinner] = useState('')
+  const [nextValue, setNextValue] = useState(calculateNextValue(squares))
+  const [status, setStatus] = useState(
+    calculateStatus(winner, squares, nextValue),
+  )
+  console.log('status', status)
+  console.log('nextValue', nextValue)
   function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
+    if (calculateWinner(squares) !== null) {
+      setWinner(calculateWinner(squares))
+      console.log(winner)
+      setStatus(calculateStatus(winner, squares, nextValue))
+      return ''
+    } else {
+      const squaresCopy = [...squares]
+      squaresCopy[square] = nextValue
+      setNextValue(calculateNextValue(squares))
+      setSquares(squaresCopy)
+      setWinner(calculateWinner(squares))
+      setNextValue(calculateNextValue(squares))
+    }
   }
 
+  useEffect(() => {
+    return () => {
+      setStatus(calculateStatus(winner, squares, nextValue))
+    }
+  }, [winner, nextValue, squares])
+
   function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
+    setSquares(Array(9).fill(null))
+    setWinner('')
   }
 
   function renderSquare(i) {
@@ -48,8 +50,7 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,12 +6,23 @@ import * as React from 'react'
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import {fetchPokemon, PokemonForm} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
+  const [pokemon, setPokemon] = React.useState(null)
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
+  React.useEffect(() => {
+    if (!pokemonName) {
+      console.log('pokemon was empty')
+      return 'Submit a pokemon'
+    }
+    setPokemon(null)
+    fetchPokemon(pokemonName).then(pokemonData => {
+      setPokemon(pokemonData)
+    })
+  }, [pokemonName, pokemon])
   // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
   // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
   // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
@@ -23,10 +34,13 @@ function PokemonInfo({pokemonName}) {
   // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
   //   1. no pokemonName: 'Submit a pokemon'
   //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  //   3. pokemon:
 
   // 💣 remove this
-  return 'TODO'
+  return ''
+  // if (pokemonName && !pokemon) {
+  //   return <PokemonInfoFallback name={pokemonName} />
+  // } else return <PokemonDataView pokemon={pokemon} />
 }
 
 function App() {
